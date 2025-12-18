70d7d026eee47c7bce27c247f2ff39246ab1a08e
Moises Velasquez <moises007@gmail.com>
2025-12-18 09:07:06 -0500

    CatchError and Resource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
 import { SearchInputComponent } from '../../../shared/components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
@@ -11,21 +13,38 @@ import type { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
+  query = signal('');
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) return [];
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
 
-  onSearch(query: string) {
-    if (this.isLoading()) return;
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
 
-    this.isLoading.set(true);
-    this.isError.set(null);
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
 
-    this.countryService.searchByCapital(query).subscribe((countries) => {
-      this.isLoading.set(false);
-      this.countries.set(countries);
-      console.log(countries);
-    });
-  }
+  //   this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //       console.log(countries);
+  //     },
+  //     error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(err);
+  //     },
+  //   });
+  // }
 }
