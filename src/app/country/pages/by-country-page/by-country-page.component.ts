542cbbe5860551ced53bf31c6977572849f7ae7c
Moises Velasquez <moises007@gmail.com>
2026-01-16 00:07:28 -0500

    preserved query by country

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,9 +1,16 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  linkedSignal,
+  resource,
+  signal,
+} from '@angular/core';
 import { firstValueFrom, of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { SearchInputComponent } from '../../../shared/components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-country-page',
@@ -12,11 +19,18 @@ import { CountryService } from '../../services/country.service';
 })
 export class ByCountryPageComponent {
   countryService = inject(CountryService);
-  query = signal('');
+  router = inject(Router);
+  activatedRouter = inject(ActivatedRoute);
+  queryParams = this.activatedRouter.snapshot.queryParamMap.get('query') ?? '';
+
+  query = linkedSignal(() => this.queryParams);
   countryResource = rxResource({
     params: () => ({ query: this.query() }),
     stream: ({ params }) => {
       if (!params.query) return of([]);
+      this.router.navigate(['/country/by-country'], {
+        queryParams: { query: params.query },
+      });
       return this.countryService.searchByCountry(params.query);
     },
   });
